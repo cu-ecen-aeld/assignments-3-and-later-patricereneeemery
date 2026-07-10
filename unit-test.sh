c55b186cded6fbbc4f33a2ec79cbdc22b59e7475
patricereneeemery <patriceemery@yahoo.com>
2026-07-09 22:06:38 -0700

    Imported Assignment 7 base files

--- unit-test.sh
+++ unit-test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Run unit tests for the assignment
 
+# Automate these steps from the readme:
+# Create a build subdirectory, change into it, run
+# cmake .. && make && run the assignment-autotest application
 mkdir -p build
 cd build
 cmake ..
 make clean
 make
 cd ..
-./build/assignment-autotest/test/assignment4/assignment-autotest
+./build/assignment-autotest/assignment-autotest
